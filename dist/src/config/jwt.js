8e6b29fc4c36fcb0e55d0cdd2e77c4e6f9fde82f
Gbotemi <gbotemiwebagency@gmail.com>
2025-09-07 20:16:48 +0100

    for callback

--- dist/src/config/jwt.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.JWT_SECRET = void 0;
-require("dotenv/config");
-exports.JWT_SECRET = process.env.JWT_SECRET || 'your_super_secret_jwt_key_please_change_me_in_env';
